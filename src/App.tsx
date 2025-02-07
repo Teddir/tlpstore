8e25d11f95696cc699634d144c7a5be9c455d39f
Teddir <ferryaja20000@gmail.com>
2025-02-07 09:22:35 +0700

    fix: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
 export default function App() {
   return (
     <div>
       <p>hallo</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
