91ca9b51fcb6dbbd0bd5bc96ca4ae407c3223afa
Teddir <ferryaja20000@gmail.com>
2025-02-07 13:19:07 +0700

    fix: update ui ux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 export default function App() {
   return (
-    <div>
-      <p>hallo</p>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+    <div className="max-w-[1024px] mx-auto bg-pink-400 w-full">
+      <h1 className="text-3xl font-bold text-center">
+        Amazon
       </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <iframe src="https://amzn.to/4aObmjE/?tag=teddir-20" className="w-full h-64"></iframe>
+        <iframe src="https://amzn.to/4aObmjE" className="w-full h-64"></iframe>
+        <iframe src="https://amzn.to/4aObmjE" className="w-full h-64"></iframe>
+        <iframe src="https://amzn.to/4aObmjE" className="w-full h-64"></iframe>
+        <iframe src="https://amzn.to/4aObmjE" className="w-full h-64"></iframe>
+        <iframe src="https://amzn.to/4aObmjE" className="w-full h-64"></iframe>
+        <iframe src="https://amzn.to/4aObmjE" className="w-full h-64"></iframe>
+        <iframe src="https://amzn.to/4aObmjE" className="w-full h-64"></iframe>
+      </div>
     </div>
-  )
+  );
 }
